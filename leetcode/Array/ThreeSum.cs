bd790679b05b282c4f9d738cb3d9fee16af6a974
Aleksandr Tarasov <alan.trsv@gmail.com>
2021-11-13 17:02:22 +0500

    try to solve tree sum (TimeLimitExceeded)

---
+++ leetcode/Array/ThreeSum.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Array
+{
+	public class Sum3
+	{
+		public IList<IList<int>> ThreeSum(int[] nums)
+		{
+			var result = new List<IList<int>>();
+			if (nums.Length < 3)
+			{
+				return result;
+			}
+			System.Array.Sort(nums);
+
+			var numsList = nums.ToList();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				TwoSumSortedAll(numsList, -nums[i], exclusion:i, result);
+			}
+
+			return result;
+		}
+
+		private void TwoSumSortedAll(List<int> nums, int target, int exclusion, List<IList<int>> result)
+		{
+			if (nums.Count < 2)
+			{
+				return;
+			}
+
+			var l = 0;
+			var r = nums.Count - 1;
+
+			while (r > l)
+			{
+				if (r == exclusion)
+				{
+					r--;
+					continue;
+				}
+				if (l == exclusion)
+				{
+					l++;
+					continue;
+				}
+
+				var twoSum = nums[l] + nums[r];
+				if (twoSum == target)
+				{
+					var newDecision = new List<int> { nums[exclusion], nums[l], nums[r] };
+					if (!result.Any(l => l.Except(newDecision).Count() == 0))
+					{
+						result.Add(newDecision);
+					}
+
+					l++;
+				}
+				else if (twoSum > target)
+				{
+					r--;
+				}
+				else
+				{
+					l++;
+				}
+			}
+
+			return;
+		}
+	}
+}
--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -21,10 +21,26 @@ namespace leetcode
 			//var nums2 = new int[] { 2, 3, 8 };
 			//var nums1 = new int[] { 3, 6, 8, 0, 0, 0, 0, 0 };
 			//var nums2 = new int[] { 1, 5, 7, 9, 15 };
-			var nums1 = new int[] { 0 };
-			var nums2 = new int[] { 1 };
-			var sln2 = new Merge_Sorted_Array();
-			sln2.Merge(nums1, 0, nums2, nums2.Length);
+			//var nums1 = new int[] { 0 };
+			//var nums2 = new int[] { 1 };
+			//var sln2 = new Merge_Sorted_Array();
+			//sln2.Merge(nums1, 0, nums2, nums2.Length);
+
+			var nums1 = new int[] { -1, 0, 1, 2, -1, -4 };
+			var nums2 = new int[] { -7, -5, -3, -2, 0, 0, 0, 1, 2, 5 };
+			var nums3 = new int[] { 0, 0, 0 };
+			var nums4 = new int[] { -1, 0, 1, 0 };
+			var nums5 = new int[0];
+			var nums6 = new int[] { -2, 0, 1, 1, 2 };
+			var nums7 = new int[]{4, -2, -9, 9, 7, 9, 10, -15, 4, -9, -9, 8, -6, 7, -7, -2, 4, -9, -7, -11, 13, 9, 5, -8, 10, 8, -6, -1, -2, -6, 6, -12, 7, 4, 4, -9, -1, -1, -8, 10, 5, -10, -5, 7, 12, 4, 12, -6, 10, -10, -2, 8, 7, 10, 7, 2, -5, 9, -14, 9, -12, -1, 4, 2, 11, -15, 9, -13, -1, -14, 4, 12, -9, -15, -4, 10, 4, -7, -11, -9, -1, -6, 14, -9, -10, -1, 0, -8, -7, -6, 8, -12, 0, -3, 5, -4, -11, -1, -10, 4, -8, 10, -7, -10, 2, 4, -14};
+			var sln = new Sum3();
+			var treeSum1 = sln.ThreeSum(nums1);
+			var treeSum2 = sln.ThreeSum(nums2);
+			var treeSum3 = sln.ThreeSum(nums3);
+			var treeSum4 = sln.ThreeSum(nums4);
+			var treeSum5 = sln.ThreeSum(nums5);
+			var treeSum6 = sln.ThreeSum(nums6);
+			var treeSum7 = sln.ThreeSum(nums7);
 
 			Console.WriteLine("Hello World!");
 		}
