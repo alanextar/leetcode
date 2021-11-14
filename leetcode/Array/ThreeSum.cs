a87b55354c30e3b7ba33ec00562cb1b8dc2d2ed4
Aleksandr Tarasov <alan.trsv@gmail.com>
2021-11-14 19:07:55 +0500

    accepted solution

--- leetcode/Array/ThreeSum.cs
+++ leetcode/Array/ThreeSum.cs
@@ -17,60 +17,43 @@ namespace leetcode.Array
 			}
 			System.Array.Sort(nums);
 
-			var numsList = nums.ToList();
-			for (int i = 0; i < nums.Length; i++)
+			for (int i = 0; i < nums.Length - 2; i++)
 			{
-				TwoSumSortedAll(numsList, -nums[i], exclusion:i, result);
+				if (i > 0 && nums[i] == nums[i - 1])
+				{
+					continue;
+				}
+				var l = i + 1;
+				var r = nums.Length - 1;
+
+				while (l < r)
+				{
+					var target = -nums[i];
+					var twoSum = nums[l] + nums[r];
+					if (twoSum == target)
+					{
+						var newDecision = new List<int> { nums[i], nums[l], nums[r] };
+						result.Add(newDecision);
+					}
+					else if (twoSum < target)
+					{
+						while (l < r && nums[l] == nums[l + 1])
+						{
+							l++;
+						}
+						l++;
+						continue;
+					}
+
+					while (r > l && nums[r] == nums[r - 1])
+					{
+						r--;
+					}
+					r--;
+				}
 			}
 
 			return result;
-		}
-
-		private void TwoSumSortedAll(List<int> nums, int target, int exclusion, List<IList<int>> result)
-		{
-			if (nums.Count < 2)
-			{
-				return;
-			}
-
-			var l = 0;
-			var r = nums.Count - 1;
-
-			while (r > l)
-			{
-				if (r == exclusion)
-				{
-					r--;
-					continue;
-				}
-				if (l == exclusion)
-				{
-					l++;
-					continue;
-				}
-
-				var twoSum = nums[l] + nums[r];
-				if (twoSum == target)
-				{
-					var newDecision = new List<int> { nums[exclusion], nums[l], nums[r] };
-					if (!result.Any(l => l.Except(newDecision).Count() == 0))
-					{
-						result.Add(newDecision);
-					}
-
-					l++;
-				}
-				else if (twoSum > target)
-				{
-					r--;
-				}
-				else
-				{
-					l++;
-				}
-			}
-
-			return;
 		}
 	}
 }
--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -33,6 +33,7 @@ namespace leetcode
 			var nums5 = new int[0];
 			var nums6 = new int[] { -2, 0, 1, 1, 2 };
 			var nums7 = new int[]{4, -2, -9, 9, 7, 9, 10, -15, 4, -9, -9, 8, -6, 7, -7, -2, 4, -9, -7, -11, 13, 9, 5, -8, 10, 8, -6, -1, -2, -6, 6, -12, 7, 4, 4, -9, -1, -1, -8, 10, 5, -10, -5, 7, 12, 4, 12, -6, 10, -10, -2, 8, 7, 10, 7, 2, -5, 9, -14, 9, -12, -1, 4, 2, 11, -15, 9, -13, -1, -14, 4, 12, -9, -15, -4, 10, 4, -7, -11, -9, -1, -6, 14, -9, -10, -1, 0, -8, -7, -6, 8, -12, 0, -3, 5, -4, -11, -1, -10, 4, -8, 10, -7, -10, 2, 4, -14};
+			var nums8 = new int[] { 0, 0, 0, 0 };
 			var sln = new Sum3();
 			var treeSum1 = sln.ThreeSum(nums1);
 			var treeSum2 = sln.ThreeSum(nums2);
@@ -41,6 +42,7 @@ namespace leetcode
 			var treeSum5 = sln.ThreeSum(nums5);
 			var treeSum6 = sln.ThreeSum(nums6);
 			var treeSum7 = sln.ThreeSum(nums7);
+			var treeSum8 = sln.ThreeSum(nums8);
 
 			Console.WriteLine("Hello World!");
 		}
