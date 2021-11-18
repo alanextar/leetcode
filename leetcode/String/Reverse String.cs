483fd64cdd817f688e934a4f925a38bad941b9ae
Aleksandr Tarasov <alan.trsv@gmail.com>
2021-11-18 16:12:42 +0500

    Reverse String easy

---
+++ leetcode/String/Reverse String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String
+{
+    public class Reverse_String
+    {
+        public void ReverseString(char[] s)
+        {
+            var i = 0;
+            var j = s.Length - 1;
+            while (i < j)
+            {
+                s.SwapValues(i, j);
+                i++;
+                j--;
+            }
+        }
+    }
+}
